130ce125a409fdb091d9d9c80545e756acd8cb9c
R45H <gmr45h@gmail.com>
2017-09-14 16:12:28 +0300

    Update 14.09.17

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,13 @@ var
    gutil        = require('gulp-util'),                    // Различные вспомогательные утилиты
    cssImport    = require('gulp-cssimport'),               // Работа @import
    strip        = require('gulp-strip-css-comments'),      // Убирает комментарии
-   path         = require('path'),                         // Для работы с путями
    runSequence  = require('run-sequence'),                 // Для синхронного выполнения задач
    pug          = require('gulp-pug'),                     // Шаблонизатор Pug (бывший Jade)
    merge        = require('gulp-merge-json'),              // Конкатенация JSON
    data         = require('gulp-data'),                    // Парс JSON
-   fs           = require('fs')                            // Чтение JSON
+   fs           = require('fs'),                           // Чтение и запись файлов
+   prettify     = require('gulp-jsbeautifier'),            // Форматирование JS и HTML
+   replace      = require('gulp-replace')                  // Замена текста в файлах
 ;
 /* ================================ */
 
@@ -43,7 +44,7 @@ var err = {
 };
 
 // Переключатель сборки для работы или в продакшн (false для работы)
-var prod = gutil.env.production;
+var prod = gutil.env.p || gutil.env.production;
 
 // Пути
 var app = 'app/'; // Папка исходников
@@ -71,7 +72,7 @@ gulp.task('json', function() {
 		.pipe(merge({ // Сольём в один
 			fileName: 'data.json'
 		}))
-		.pipe(gulp.dest('temp/')); // Выплюнем
+		.pipe(gulp.dest(temp)); // Выплюнем
 });
 /* ================================ */
 
@@ -82,16 +83,18 @@ gulp.task('pug', function () {
 		.pipe(data(function(file) { // Парсим JSON
 			return JSON.parse(fs.readFileSync('temp/data.json'))
 		}))
-		.pipe(prod ? pug({ // Сконвертим в HTML
-			pretty: '\t' // Форматируем с табами вместо пробелов
-		}) : pug())
+		.pipe(pug()) // Сконвертим в HTML
+		.pipe(prod ? prettify({ // Форматируем код
+			indent_char: '\t',
+			indent_size: 1
+		}) : gutil.noop())
 		.pipe(gulp.dest(dist)) // Выплюнем их
 		.pipe(reload({stream: true})); //Перезагрузим сервер
 });
 /* ================================ */
 
 /* ========= ТАСК "SASS" ========== */
-gulp.task('sass', function() {
+gulp.task('scss', function() {
 	return gulp.src(app + 'src/style.scss') // Берём источник
 		.pipe(plumber(err)) // Отслеживаем ошибки
 		.pipe(cssImport()) // Запускаем @import
@@ -120,9 +123,16 @@ gulp.task('css-libs', function() {
 
 /* ======== ТАСК "JS" ======== */
 gulp.task('js', function() {
-	return gulp.src(app + 'src/script.js') // Берём все необходимые скрипты
+	return gulp.src([ // Берём файлы
+		app + 'src/script.js',
+		app + 'src/blocks/map/map.js'
+	])
 		.pipe(plumber(err)) // Отслеживаем ошибки
 		.pipe(include()) // Собираем их в один файл
+		.pipe(prod ? prettify({ // Форматируем код
+			indent_char: '\t',
+			indent_size: 1
+		}) : gutil.noop())
 		.pipe(gulp.dest(dist + 'js')) // Выгружаем
 		.pipe(reload({stream: true})); // Перезагружаем сервер
 });
@@ -164,7 +174,7 @@ gulp.task('fonts', function() {
 
 /* ========= ТАСК "CLEAN" ========= */
 gulp.task('clean', function() {
-	return del.sync([dist, temp]); // Удаляем папку "dist" перед сборкой
+	return del.sync([dist, temp]); // Удаляем папки "dist" и "temp" перед сборкой
 });
 /* ================================ */
 
@@ -175,7 +185,7 @@ gulp.task('build', function(callback) {
 		'json',
 		[
 			'pug',
-			'sass',
+			'scss',
 			'css-libs',
 			'js',
 			'js-libs',
@@ -190,7 +200,7 @@ gulp.task('build', function(callback) {
 /* ========= ТАСК "WATCH" ========= */
 gulp.task('watch', function() {
 	gulp.watch(app + '**/*.pug', ['pug']); // Наблюдение за PUG файлами
-	gulp.watch([app + 'src/**/*.scss', '!' + app + 'src/libs.scss'], ['sass']); // Наблюдение за своими SCSS файлами
+	gulp.watch([app + 'src/**/*.scss', '!' + app + 'src/libs.scss'], ['scss']); // Наблюдение за своими SCSS файлами
 	gulp.watch(app + 'src/libs.scss', ['css-libs']); // Наблюдение за скачанными CSS файлами
 	gulp.watch([app + 'src/**/*.js', '!' + app + 'src/libs.js'], ['js']); // Наблюдение за своими JS файлами
 	gulp.watch(app + 'src/libs.js', ['js-libs']); // Наблюдение за скачанными JS файлами
@@ -238,5 +248,236 @@ gulp.task('sprite', function() {
 
 	spriteData.img.pipe(gulp.dest(app + 'sprites/')); // путь, куда сохраняем картинку
 	spriteData.css.pipe(gulp.dest(app + 'sprites/')); // путь, куда сохраняем стили
+});
+/* ================================ */
+
+/* ====== СОЗДАНИЕ СТРАНИЦЫ ======= */
+/**
+ * name   (n) - Имя файла
+ * title  (t) - Заголовок страницы
+ * layout (l) - Шаблон страницы
+ */
+gulp.task('page', function() {
+	var
+		name = gutil.env.name || gutil.env.n || 'blank',
+		title = gutil.env.title || gutil.env.t || 'Пустая страница',
+		layout = gutil.env.layout || gutil.env.l || 'default',
+		string =
+			'extends layouts/' + layout + '\n' +
+			'\n' +
+			'block vars\n' +
+			'\t-\n' +
+			'\t\tpage = {\n' +
+			'\t\t\ttitle: \'' + title + '\',\n' +
+			'\t\t\tlink: \'' + name + '.html\'\n' +
+			'\t\t}\n' +
+			'\n' +
+			'block content\n' +
+			'\tinclude pages/' + name;
+
+	fs.writeFileSync(app + 'templates/' + name + '.pug', string);
+	fs.writeFileSync(app + 'templates/pages/' + name + '.pug', '');
+});
+/* ================================ */
+
+/* ======= СОЗДАНИЕ БЛОКА ========= */
+/**
+ * name                        (n) - Имя блока
+ * scss                        (s) - Генерация SCSS
+ * js                          (j) - Генерация JS
+ * mixins, mixin, mix          (m) - Генерация PUG миксина
+ * components, component, comp (c) - Генерация PUG компонента
+ * partials, partial, part     (p) - Генерация PUG части страницы
+ * json                        (o) - Генерация данных JSON
+ */
+gulp.task('block', function() {
+	var
+		name = gutil.env.name || gutil.env.n, // Имя блока
+
+		dirBlocks = app + 'src/blocks/', // Полный путь до папки с блоками
+		dirTemp = app + 'templates/', // Полный путь до папки с вёрсткой
+
+		dirThis = dirBlocks + name + '/', // Полный путь до папки с текущим блоком
+		dirThisRel = 'blocks/' + name + '/', // Относительный путь до папки с текущим блоком
+
+		keyScss = gutil.env.scss || gutil.env.s, // Ключ генерации SCSS файла
+		keyJs = gutil.env.js || gutil.env.j, // Ключ генерации JS файла
+		keyPugMixin = gutil.env.mixins || gutil.env.mixin || gutil.env.mix || gutil.env.m, // Ключ генерации PUG миксина
+		keyPugComp = gutil.env.components || gutil.env.component || gutil.env.comp || gutil.env.c, // Ключ генерации PUG компонента
+		keyPugPart = gutil.env.partials || gutil.env.partial || gutil.env.part || gutil.env.p, // Ключ генерации PUG части страницы
+		keyDataJson = gutil.env.json || gutil.env.o; // Ключ генерации JSON
+
+	// Генерация SCSS при запуске без ключей
+	if (!keyScss && !keyJs && !keyPugMixin && !keyPugComp && !keyPugPart && !keyDataJson) {
+
+		fs.access(dirBlocks + name + '.js', function(err) {
+
+			if (err) {
+				addScss(dirBlocks, 'blocks/');
+			} else {
+				moveJsToFolder();
+				addScss(dirThis, dirThisRel);
+			}
+		});
+	}
+	// =====
+
+	// Генерация SCSS и JS файлов
+	if (keyScss) {
+
+		if (keyJs) {
+			fs.mkdirSync(dirThis);
+			addScss(dirThis, dirThisRel);
+			addJs(dirThis, dirThisRel);
+		} else {
+
+			fs.access(dirBlocks + name + '.js', function(err) {
+
+				if (err) {
+					addScss(dirBlocks, 'blocks/');
+				} else {
+					moveJsToFolder();
+					addScss(dirThis, dirThisRel);
+				}
+			});
+		}
+	} else {
+
+		if (keyJs) {
+
+			fs.access(dirBlocks + name + '.scss', function(err) {
+
+				if (err) {
+					addJs(dirBlocks, 'blocks/');
+				} else {
+					moveScssToFolder();
+					addJs(dirThis, dirThisRel);
+				}
+			});
+		}
+	}
+	// =====
+
+	// Генерация PUG миксина
+	if (keyPugMixin) {
+		addPugMixin(dirTemp);
+	}
+	// =====
+
+	// Генерация PUG компонента
+	if (keyPugComp) {
+		addPugComp(dirTemp);
+	}
+	// =====
+
+	// Генерация PUG части страницы
+	if (keyPugPart) {
+		addPugPart(dirTemp);
+	}
+	// =====
+
+	// Генерация JSON файла
+	if (keyDataJson) {
+		addDataJson(dirTemp);
+	}
+	// =====
+
+	function addScss(path, relPath) {
+		var
+			str =
+				'$name: ' + name + ';\n' +
+				'\n' +
+				'.#{$name} {\n' +
+				'\t\n' +
+				'}',
+			pathToMain = app + 'src/style.scss',
+			inc = '\n@import url(\'' + relPath + name + '.scss\');';
+
+		fs.writeFileSync(path + name + '.scss', str);
+		fs.appendFileSync(pathToMain, inc);
+	}
+	function addJs(path, relPath) {
+
+		fs.writeFileSync(
+			path + name + '.js',
+			''
+		);
+
+		fs.appendFileSync(
+			app + 'src/script.js',
+			'\n(function() { @@include(\'' + relPath + name + '.js\') }());'
+		);
+	}
+	function addPugMixin(path) {
+
+		fs.writeFileSync(
+			path + 'mixins/' + name + '.pug',
+			'mixin ' + name + '(data)\n\t'
+		);
+	}
+	function addPugComp(path) {
+
+		fs.writeFileSync(
+			path + 'components/' + name + '.pug',
+			''
+		);
+	}
+	function addPugPart(path) {
+
+		fs.writeFileSync(
+			path + 'partials/' + name + '.pug',
+			''
+		);
+	}
+	function addDataJson(path) {
+		name = name.replace(new RegExp('-', 'g'), '_');
+
+		var str =
+			'{\n' +
+			'\t"' + name + '": [\n' +
+			'\t\t{\n' +
+			'\t\t\t\n' +
+			'\t\t}\n' +
+			'\t]\n' +
+			'}';
+
+		fs.writeFileSync(path + 'data/' + name + '.json', str);
+	}
+	function moveJsToFolder() {
+
+		fs.mkdirSync(dirThis);
+
+		gulp.src(dirBlocks + name + '.js')
+			.pipe(gulp.dest(dirThis));
+
+		gulp.src(app + 'src/script.js')
+			.pipe(replace(
+				'@@include(\'blocks/' + name + '.js\')',
+				'@@include(\'' + dirThisRel + name + '.js\')'
+			))
+			.pipe(gulp.dest(function(file) {
+				return file.base;
+			}));
+
+		return del('./' + dirBlocks + name + '.js');
+	}
+	function moveScssToFolder() {
+
+		fs.mkdirSync(dirThis);
+
+		gulp.src(dirBlocks + name + '.scss')
+			.pipe(gulp.dest(dirThis));
+
+		gulp.src(app + 'src/style.scss')
+			.pipe(replace(
+				'@import url(\'blocks/' + name + '.scss\');',
+				'@import url(\'' + dirThisRel + name + '.scss\');'
+			))
+			.pipe(gulp.dest(function(file) {
+				return file.base;
+			}));
+
+		return del('./' + dirBlocks + name + '.scss');
+	}
 });
 /* ================================ */
