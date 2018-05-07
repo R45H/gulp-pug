54e7cdff9bbc4c85f5203f139304b4a84f654577
R45H <gmr45h@gmail.com>
2018-05-07 13:53:39 +0300

    Update bootstrap and some fixes

--- app/src/global/global.js
+++ app/src/global/global.js
@@ -1,5 +1,16 @@
-$("a[href='#']").on('click', function(event) {
-	event.preventDefault();
+// Для ссылок с href='#' и наличием только атрибутов class, href и style отключается стандартное поведение ссылки
+$(document).on('click', 'a', function(event) {
+	var $this = $(this);
+
+	if ($this.attr('href') != '#') return;
+
+	if (
+		(this.attributes.length < 4 && $this.attr('class') && $this.attr('href') && $this.attr('style')) ||
+		(this.attributes.length < 3 && $this.attr('class') && $this.attr('href')) ||
+		(this.attributes.length < 2 && !$this.attr('class') && $this.attr('href'))
+	) {
+		event.preventDefault();
+	}
 });
 
 $('.js-print').on('click', function() {
